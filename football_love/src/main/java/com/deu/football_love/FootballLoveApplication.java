d8d8ceb8dfb49aede19f9049116842db18848fbf
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-09-27 19:15:57 +0900

    add be

---
+++ football_love/src/main/java/com/deu/football_love/FootballLoveApplication.java
@@ -0,0 +1,13 @@
+package com.deu.football_love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLoveApplication.class, args);
+	}
+
+}
---
+++ football_love/src/test/java/com/deu/football_love/FootballLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deu.football_love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
