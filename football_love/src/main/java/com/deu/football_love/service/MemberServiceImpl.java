46d4fd20f8c3868a73810ec414e7a56fddd6feec
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-10-18 11:41:01 +0900

    [feat]회원 조회 함수 추가

--- football_love/src/main/java/com/deu/football_love/service/MemberServiceImpl.java
+++ football_love/src/main/java/com/deu/football_love/service/MemberServiceImpl.java
@@ -43,6 +43,11 @@ public class MemberServiceImpl implements MemberService {
 		}
 	}
 
+	@Override
+	@Transactional(readOnly = true)
+	public Member findMember(String id) {
+		return memberRepository.selectMember(id,"123");
+	}
 
 
 }
