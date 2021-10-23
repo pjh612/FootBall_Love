74d595adfa3c8ef69208519201ccb987861ada71
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-10-23 11:47:07 +0900

    [feat]DB 설계 변경으로 인한 Entity 추가(매치 참가 신청, 매치 참여 팀원, 탈퇴 회원 목록)

---
+++ football_love/src/main/java/com/deu/football_love/domain/MatchApplication.java
@@ -0,0 +1,23 @@
+package com.deu.football_love.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class MatchApplication {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "match_application_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "matches_id")
+    private Matches matches;
+
+    @Column(name = "applicaiton_approval")
+    private Boolean approval;
+}
