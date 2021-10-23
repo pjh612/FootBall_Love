05a6f41adf1abaa7e957e494311b19535d526a47
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-10-23 11:50:41 +0900

    [fix]import 변경

--- football_love/src/main/java/com/deu/football_love/domain/Member.java
+++ football_love/src/main/java/com/deu/football_love/domain/Member.java
@@ -5,14 +5,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
