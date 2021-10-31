74c9c954ba54bb84e924f907299cd3607ad3a3c9
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-10-31 15:40:09 +0900

    [feat]팀 조회, 팀 멤버조회, 팀 어드민 조회, 팀 가입신청 조회, 팀 가입 신청, 팀 가입, 팀 멤버 삭제, 팀 삭제 Repository 함수 작성

--- football_love/src/main/java/com/deu/football_love/repository/TeamRepository.java
+++ football_love/src/main/java/com/deu/football_love/repository/TeamRepository.java
@@ -1,13 +1,22 @@
 package com.deu.football_love.repository;
 
+import com.deu.football_love.domain.ApplicationJoinTeam;
 import com.deu.football_love.domain.Team;
 import com.deu.football_love.domain.TeamAdmin;
 import com.deu.football_love.domain.TeamMember;
 
+import java.util.List;
+
 public interface TeamRepository {
 
     void insertTeam(TeamAdmin teamAdmin, Team team);
-    Team selectTeam(Long id);
-    Team selectTeamByName(String teamName);
+    Team selectTeam(String teamName);
+    void insertNewApplication(ApplicationJoinTeam application);
     void insertNewTeamMember(TeamMember newTeamMember);
+    ApplicationJoinTeam selectApplication(String teamName, String memberId);
+    void deleteApplication(ApplicationJoinTeam application);
+    void deleteTeamMember(String teamName, String memberId);
+    List<TeamMember> selectTeamMember(String teamName, String memberId);
+    List<TeamAdmin> selectTeamAdmin(String teamName, String memberId);
+    void deleteTeam(Team team);
 }
