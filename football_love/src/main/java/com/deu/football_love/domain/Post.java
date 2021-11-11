b420877f8be8f06f8658f4d65cb7841ab0572ba3
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-11-11 20:03:21 +0900

    [feat]생성자 추가, 게시물 update 함수 추가

--- football_love/src/main/java/com/deu/football_love/domain/Post.java
+++ football_love/src/main/java/com/deu/football_love/domain/Post.java
@@ -1,9 +1,15 @@
 package com.deu.football_love.domain;
 
+import com.deu.football_love.dto.UpdatePostRequest;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Post {
 
     @Column(name= "post_id")
@@ -31,5 +37,22 @@ public class Post {
     @Column(name= "post_content")
     private String content;
 
+    protected Post() {
+    }
 
+    public Post(Member author, Board board, LocalDateTime createDate, LocalDateTime modifyDate, String title, String content) {
+        this.author = author;
+        this.board = board;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(UpdatePostRequest request)
+    {
+        this.setTitle(request.getTitle());
+        this.setModifyDate(request.getModifyDate());
+        this.setContent(request.getContent());
+    }
 }
