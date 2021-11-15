0185c5face3e7d58c192cb704509ed9217fd5b58
ByeonggakYu <qudrkr0815@korea.ac.kr>
2021-11-15 11:54:18 +0900

    [feat]Slide 구현중

--- front/src/Carousel.jsx
+++ front/src/Carousel.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 import styled from "styled-components";
 
-import slider from './slider';
-
 const CarouselContainer = styled.div`
     background-color : #fafafa;
     margin: 0 auto;
     border-bottom: 1px solid #ddd;
 `;
 
-const Test = styled.div`
-    height: 100px;
-    background-color: yellow;
+const ParentDiv = styled.div`
+  background-color: #fafafa;
+  margin: 0 auto;
+  border-bottom: 1px solid #ddd;
+  position: relative;
+  display: flex;
+  justify-content: center;
 `;
 
+const ImageDiv = styled.div`
+  width: 600px;
+  height: 400px;
+  margin: 20 auto;
+  overflow: hidden;
+  display: flex;
+`;
 
-
+// 이어서 할것: 버튼 배치하고 슬라이드
 export default function Carousel() {
     const imgsrc = ['img/football.jpg', 'img/football2.jpg'];
-    const newDiv = slider(imgsrc, 500, 500);
-    console.log(newDiv);
+    const imgStyle = {
+        width: 600,
+        height: 400,
+    }
+    const buttonNum = imgsrc.length;
+    const buttonStyle = {
+    }
+
     return(
         <CarouselContainer>
-            <Test></Test>
+           <ParentDiv>
+               <ImageDiv>
+                   {imgsrc.map((src) => {
+              return <img src={src} style={imgStyle}></img>;
+            })}
+                {[...Array(buttonNum)].map((n, index) => {
+                    return <button style={buttonStyle}>{index + 1}</button>   
+                })
+                }
+               </ImageDiv>
+           </ParentDiv>
         </CarouselContainer>
     )
 }
