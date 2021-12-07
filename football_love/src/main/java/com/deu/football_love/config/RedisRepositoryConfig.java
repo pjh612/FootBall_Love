e53f1687d47b43b0fffc84e7468fbafe8b7ff1d0
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-12-07 22:14:08 +0900

    [feat]redis config 추가

--- football_love/src/main/java/com/deu/football_love/config/RedisRepositoryConfig.java
+++ football_love/src/main/java/com/deu/football_love/config/RedisRepositoryConfig.java
@@ -1,26 +1,35 @@
 package com.deu.football_love.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisRepositoryConfig {
 
     private final RedisProperties redisProperties;
+    @Value("${spring.redis.host}")
+    private String host;
 
+    @Value("${spring.redis.port}")
+    private int port;
     // lettuce
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
@@ -31,4 +40,15 @@ public class RedisRepositoryConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
+    private final String PATTERN = "__keyevent@*__:expired";
+
+    /*@Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory, RedisExpirationListener expirationListener) {
+        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
+        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
+        redisMessageListenerContainer.addMessageListener(expirationListener, new PatternTopic(PATTERN));
+        redisMessageListenerContainer.setErrorHandler(e -> log.error("There was an error in redis key expiration listener container", e));
+        return redisMessageListenerContainer;
+    }*/
 }
