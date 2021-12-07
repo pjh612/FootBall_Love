83bb902d0686641c9ad4d303eb0ba85dac9c455d
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-12-07 22:07:51 +0900

    [refactor]코드 스타일 리팩토링

--- football_love/src/main/java/com/deu/football_love/FootballLoveApplication.java
+++ football_love/src/main/java/com/deu/football_love/FootballLoveApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FootballLoveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FootballLoveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FootballLoveApplication.class, args);
+    }
 }
