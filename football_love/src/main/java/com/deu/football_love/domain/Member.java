4cbd2be4f5051924f6c982b011f1c01a0926f3b9
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-12-07 22:01:47 +0900

    [refacotr]코드 스타일 리팩토링

--- football_love/src/main/java/com/deu/football_love/domain/Member.java
+++ football_love/src/main/java/com/deu/football_love/domain/Member.java
@@ -8,82 +8,78 @@ import java.util.List;
 import javax.persistence.*;
 
 import com.deu.football_love.domain.type.MemberType;
-import com.deu.football_love.dto.BaseDto;
-import com.deu.football_love.dto.UpdateMemberRequest;
-import org.springframework.data.annotation.CreatedDate;
+import com.deu.football_love.dto.member.UpdateMemberRequest;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
 @Setter
 public class Member extends BaseEntity {
 
-	@Id @GeneratedValue
-	@Column(name ="member_number")
-	private Long number;
+    @Id
+    @GeneratedValue
+    @Column(name = "member_number")
+    private Long number;
 
 	@Column(name = "member_id", length = 20)
 	private String id;
 
-	@Column(name = "member_pwd", length = 150)
-	private String pwd;
+    @Column(name = "member_pwd", length = 150)
+    private String pwd;
 
-	@Column(name = "member_nickname", length = 20)
-	private String nickname;
+    @Column(name = "member_nickname", length = 20)
+    private String nickname;
 
-	@Column(name = "member_name", length = 20)
-	private String name;
+    @Column(name = "member_name", length = 20)
+    private String name;
 
-	@Column(name = "member_birth")
-	private LocalDate birth;
+    @Column(name = "member_birth")
+    private LocalDate birth;
 
-	@Column(name = "member_address")
-	@Embedded
-	private Address address;
+    @Column(name = "member_address")
+    @Embedded
+    private Address address;
 
 	@Column(name = "member_email")
 	private String email;
 
-	@Column(name = "member_phone")
-	private String phone;
+    @Column(name = "member_phone")
+    private String phone;
 
-	@Column(name = "member_creatdate")
-	private LocalDateTime createDate;
+    @Column(name = "member_creatdate")
+    private LocalDateTime createDate;
 
-	@Column(name = "member_type")
-	private MemberType memberType;
+    @Column(name = "member_type")
+    private MemberType memberType;
 
-	@OneToMany(mappedBy = "author")
-	private List<Post> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts = new ArrayList<>();
 
-	@OneToMany(mappedBy = "member" )
-	private List<TeamMember> teamMembers = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<TeamMember> teamMembers = new ArrayList<>();
 
-	@OneToMany(mappedBy = "member")
-	private List<ParticipationMember> participationMembers = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<ParticipationMember> participationMembers = new ArrayList<>();
 
-	@OneToMany(mappedBy = "member")
-	private List<ApplicationJoinTeam> applicationJoinTeams = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<ApplicationJoinTeam> applicationJoinTeams = new ArrayList<>();
 
-	@OneToOne(mappedBy = "member")
-	private WithdrawalMember withdrawalMember;
+    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL)
+    private WithdrawalMember withdrawalMember;
 
-	@OneToOne(mappedBy = "owner")
-	private Company company;
+    @OneToOne(mappedBy = "owner")
+    private Company company;
 
 
-	public void updateMember(UpdateMemberRequest request)
-	{
-		pwd = request.getPwd();
-		email = request.getEmail();
-		name = request.getName();
-		nickname = request.getNickname();
-		birth = request.getBirth();
-		address = request.getAddress();
-		phone = request.getPhone();
-	}
+    public void updateMember(UpdateMemberRequest request) {
+        pwd = request.getPwd();
+        email = request.getEmail();
+        name = request.getName();
+        nickname = request.getNickname();
+        birth = request.getBirth();
+        address = request.getAddress();
+        phone = request.getPhone();
+    }
 }
