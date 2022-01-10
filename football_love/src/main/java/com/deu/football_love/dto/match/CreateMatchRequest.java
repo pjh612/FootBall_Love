9708313a7442fe1a7392e2b150a9df8f0b0bf3d2
유시준 <simba0502@naver.com>
2022-01-10 23:32:32 +0900

    [feat] Match 유효성 검사 추가

--- football_love/src/main/java/com/deu/football_love/dto/match/CreateMatchRequest.java
+++ football_love/src/main/java/com/deu/football_love/dto/match/CreateMatchRequest.java
@@ -4,9 +4,23 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Getter
 public class CreateMatchRequest {
+	@Positive
+	@NotNull
     private Long teamId;
+	
+	@Positive
+	@NotNull
     private Long stadiumId;
+	
+	@Future
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd", timezone = "Asia/Seoul")
     private LocalDateTime reservation_time;
 }
--- football_love/src/main/java/com/deu/football_love/dto/match/ModifyMatchRequest.java
+++ football_love/src/main/java/com/deu/football_love/dto/match/ModifyMatchRequest.java
@@ -2,14 +2,29 @@ package com.deu.football_love.dto.match;
 
 import java.time.LocalDateTime;
 
-import com.deu.football_love.domain.Stadium;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 
 @Getter
 public class ModifyMatchRequest {
+	@Positive
+	@NotNull
 	Long matchId;
+	
+	@Positive
+	@NotNull
 	Long teamId;
+	
+	@Positive
+	@NotNull
 	Long stadiumId;
+
+	@Future
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd", timezone = "Asia/Seoul")
 	LocalDateTime reservationTime;
 }
