22ffc1576408101f937fde6b96f8f2da90d98ffe
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-01-26 00:15:12 +0900

    [fix]게시물 생성 요청 시 teamId도 포함하도록 변경

--- football_love/src/main/java/com/deu/football_love/dto/post/WritePostRequest.java
+++ football_love/src/main/java/com/deu/football_love/dto/post/WritePostRequest.java
@@ -1,9 +1,12 @@
 package com.deu.football_love.dto.post;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-
-import lombok.Data;
+import java.util.List;
 
 @Data
 public class WritePostRequest {
@@ -15,6 +18,10 @@ public class WritePostRequest {
 	@NotNull
 	private Long boardId;
 
+	@Positive
+	@NotNull
+	private Long teamId;
+
 	@NotNull
 	@Size(min = 1, max = 30)
 	private String title;
@@ -22,11 +29,13 @@ public class WritePostRequest {
 	@NotNull
 	@Size(min = 1, max = 30)
 	private String content;
-  private List<MultipartFile> images;
 
-	   public WritePostRequest(Long authorNumber, Long boardId, String title, String content, List<MultipartFile> images) {
+  	private List<MultipartFile> images;
+
+	   public WritePostRequest(Long authorNumber, Long boardId, Long teamId, String title, String content, List<MultipartFile> images) {
         this.authorNumber = authorNumber;
         this.boardId = boardId;
+        this.teamId = teamId;
         this.title = title;
         this.content = content;
         this.images = images;
