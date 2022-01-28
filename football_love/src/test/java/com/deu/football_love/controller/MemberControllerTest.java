e850aa4dccc3c003883883e2d805e967f564b5f8
유시준 <simba0502@naver.com>
2022-01-28 21:58:16 +0900

    [feat] Member Controller테스트 작성

--- football_love/src/test/java/com/deu/football_love/controller/MemberControllerTest.java
+++ football_love/src/test/java/com/deu/football_love/controller/MemberControllerTest.java
@@ -1,171 +1,188 @@
 package com.deu.football_love.controller;
 
-import com.deu.football_love.domain.Address;
-import com.deu.football_love.domain.type.MemberType;
-import com.deu.football_love.dto.auth.LoginRequest;
-import com.deu.football_love.dto.member.MemberJoinRequest;
-import com.deu.football_love.dto.member.QueryMemberDto;
-import com.deu.football_love.dto.team.CreateTeamRequest;
-import com.deu.football_love.dto.team.QueryTeamDto;
-import com.deu.football_love.repository.MemberRepository;
-import com.deu.football_love.service.MemberService;
-import com.deu.football_love.service.TeamService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.time.LocalDate;
+import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-
-import java.time.LocalDate;
+import com.deu.football_love.domain.Address;
+import com.deu.football_love.domain.type.MemberType;
+import com.deu.football_love.dto.auth.LoginRequest;
+import com.deu.football_love.dto.member.MemberJoinRequest;
+import com.deu.football_love.dto.member.QueryMemberDto;
+import com.deu.football_love.service.MemberService;
+import com.deu.football_love.service.TeamService;
+import com.deu.football_love.service.redis.RedisService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 class MemberControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-    @Autowired
-    private MemberService memberService;
+  @Autowired
+  private MemberService memberService;
 
-    @Autowired
-    private TeamService teamService;
+  @Autowired
+  private TeamService teamService;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-    
-    @Autowired
-    private ObjectMapper mapper;
+  @Autowired
+  private UserDetailsService userDetailsService;
 
-    private static final String BASE_URL = "/api/member";
-    
-    @DisplayName("정상적인 회원가입 테스트")
-    @Test
-    public void join() throws Exception {
-        Address address = new Address("양산", "서들4길", "18");
-        MemberJoinRequest request = new MemberJoinRequest("dbtlwns","123","ggongchi","유시준",
-               LocalDate.of(1995, 05, 02), address, "simba0502@naver.com","010-6779-3476", MemberType.NORMAL);
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request))
-                .with(csrf())
-        ).andExpect(status().isOk())
-                .andExpect(jsonPath("$.id",is(request.getId())))
-                .andExpect( jsonPath("$.name",is(request.getName())))
-                .andExpect( jsonPath("$.nickname",is(request.getNickname())))
-                .andExpect( jsonPath("$.address.city",is(request.getAddress().getCity())))
-                .andExpect( jsonPath("$.address.street",is(request.getAddress().getStreet())))
-                .andExpect( jsonPath("$.address.zipcode",is(request.getAddress().getZipcode())))
-                .andExpect( jsonPath("$.birth",is("1995-05-02")))
-                .andExpect( jsonPath("$.email",is(request.getEmail())))
-                .andExpect( jsonPath("$.phone",is(request.getPhone())))
-                .andExpect( jsonPath("$.type",is("NORMAL")))
-                .andDo(print());
-    }
+  @Autowired
+  private RedisService redisService;
 
-    @DisplayName("파라미터중 null이 있는 회원가입 테스트")
-    @Test
-    public void testNotNullJoin() throws Exception {
-        Address address = new Address("양산", "서들4길", "18");
-        MemberJoinRequest request = new MemberJoinRequest("dbtlwns","123","ggongchi",null,
-               LocalDate.of(1995, 05, 02), address, "simba0502@naver.com","010-6779-3476", MemberType.NORMAL);
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request))
-                .with(csrf())
-        ).andExpect(status().isBadRequest())
-                .andDo(print());
-    }
-    
-    @DisplayName("휴대폰 번호의 정규표현식이 알맞지 않은 테스트")
-    @Test
-    public void testPhoneRegexpJoin() throws Exception {
-        Address address = new Address("양산", "서들4길", "18");
-        MemberJoinRequest request = new MemberJoinRequest("dbtlwns","123","ggongchi","유시준",
-               LocalDate.of(1995, 05, 02), address, "simba0502@naver.com","01067793476", MemberType.NORMAL);
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request))
-                .with(csrf())
-        ).andExpect(status().isBadRequest())
-                .andDo(print());
-    }
-    
-    @DisplayName("빈 문자열이 들어오는 테스트")
-    @Test
-    public void testEmptyStringArgsJoin() throws Exception {
-        Address address = new Address("양산", "서들4길", "18");
-        MemberJoinRequest request = new MemberJoinRequest("","123","ggongchi","유시준",
-               LocalDate.of(1995, 05, 02), address, "simba0502@naver.com","01067793476", MemberType.NORMAL);
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request))
-                .with(csrf())
-        ).andExpect(status().isBadRequest())
-                .andDo(print());
-    }
-    
-    @Test
-    public void login() throws Exception {
-        Address address = new Address("busan", "guemgangro", "46233");
-        mapper.registerModule(new JavaTimeModule());
-        MemberJoinRequest request = new MemberJoinRequest("pjh612","123","jhjh","박진형",
-                null, address, "pjh_jn@naver.com","01021042419", MemberType.NORMAL);
+  @Autowired
+  private ObjectMapper mapper;
 
-        LoginRequest loginRequest = new LoginRequest(request.getId(), request.getPwd());
-        memberService.join(request);
+  private static final String BASE_URL = "/api/member";
 
-        mvc.perform(MockMvcRequestBuilders.post(BASE_URL+"/login_jwt/"+ request.getId())
-                .contentType("application/json")
-                .content(mapper.writeValueAsString(loginRequest))
-                .with(csrf())
-        ).andExpect(status().isOk())
-                .andDo(print());
-    }
+  private static final String ACCESS_TOKEN = "accessToken";
 
-    @Test
-    public void createTeam() throws Exception {
+  private static final String REFRESH_TOKEN = "refreshToken";
 
-        Address address = new Address("busan", "guemgangro", "46233");
-        mapper.registerModule(new JavaTimeModule());
-        MemberJoinRequest request = new MemberJoinRequest("pjh612","123","jhjh","박진형",
-                null, address, "pjh_jn@naver.com","01021042419", MemberType.NORMAL);
-        QueryMemberDto join = memberService.join(request);
-        CreateTeamRequest createTeamRequest = new CreateTeamRequest("FC진형");
-        UserDetails userDetails = userDetailsService.loadUserByUsername(join.getId());
-        mvc.perform(MockMvcRequestBuilders.post("/team").with(user(userDetails))
-                .contentType("application/json")
-                .content(mapper.writeValueAsString(createTeamRequest))
-                .with(csrf())
-        ).andExpect(status().isOk())
-                .andDo(print());
+  @DisplayName("정상적인 회원가입 테스트")
+  @Test
+  public void join() throws Exception {
+    Address address = new Address("양산", "서들4길", "18");
+    MemberJoinRequest request =
+        new MemberJoinRequest("dbtlwns", "123", "ggongchi", "유시준", LocalDate.of(1995, 05, 02),
+            address, "simba0502@naver.com", "010-6779-3476", MemberType.NORMAL);
+    mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(request)).with(csrf())).andExpect(status().isOk())
+        .andExpect(jsonPath("$.id", is(request.getId())))
+        .andExpect(jsonPath("$.name", is(request.getName())))
+        .andExpect(jsonPath("$.nickname", is(request.getNickname())))
+        .andExpect(jsonPath("$.address.city", is(request.getAddress().getCity())))
+        .andExpect(jsonPath("$.address.street", is(request.getAddress().getStreet())))
+        .andExpect(jsonPath("$.address.zipcode", is(request.getAddress().getZipcode())))
+        .andExpect(jsonPath("$.birth", is("1995-05-02")))
+        .andExpect(jsonPath("$.email", is(request.getEmail())))
+        .andExpect(jsonPath("$.phone", is(request.getPhone())))
+        .andExpect(jsonPath("$.type", is("NORMAL"))).andDo(print());
+  }
 
-        QueryTeamDto findTeam = teamService.findTeamByName(createTeamRequest.getName());
-        QueryMemberDto memberA = memberService.findMemberById("pjh612");
-        assertNotNull(findTeam);
-        assertEquals(1, teamService.findTeamMember(findTeam.getId(), memberA.getNumber()).size());
-    }
+  @DisplayName("파라미터중 null이 있는 회원가입 테스트")
+  @Test
+  public void testNotNullJoin() throws Exception {
+    Address address = new Address("양산", "서들4길", "18");
+    MemberJoinRequest request =
+        new MemberJoinRequest("dbtlwns", "123", "ggongchi", null, LocalDate.of(1995, 05, 02),
+            address, "simba0502@naver.com", "010-6779-3476", MemberType.NORMAL);
+    mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(request)).with(csrf()))
+        .andExpect(status().isBadRequest()).andDo(print());
+  }
 
-   /* @Test
-    public void */
+  @DisplayName("휴대폰 번호의 정규표현식이 알맞지 않은 테스트")
+  @Test
+  public void testPhoneRegexpJoin() throws Exception {
+    Address address = new Address("양산", "서들4길", "18");
+    MemberJoinRequest request =
+        new MemberJoinRequest("dbtlwns", "123", "ggongchi", "유시준", LocalDate.of(1995, 05, 02),
+            address, "simba0502@naver.com", "01067793476", MemberType.NORMAL);
+    mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(request)).with(csrf()))
+        .andExpect(status().isBadRequest()).andDo(print());
+  }
 
+  @DisplayName("빈 문자열이 들어오는 테스트")
+  @Test
+  public void testEmptyStringArgsJoin() throws Exception {
+    Address address = new Address("양산", "서들4길", "18");
+    MemberJoinRequest request =
+        new MemberJoinRequest("", "123", "ggongchi", "유시준", LocalDate.of(1995, 05, 02), address,
+            "simba0502@naver.com", "01067793476", MemberType.NORMAL);
+    mvc.perform(MockMvcRequestBuilders.post(BASE_URL).contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(request)).with(csrf()))
+        .andExpect(status().isBadRequest()).andDo(print());
+  }
+
+  @DisplayName("로그인,로그아웃 테스트")
+  @Test
+  public void loginTest() throws Exception {
+    Address address = new Address("busan", "guemgangro", "46233");
+    mapper.registerModule(new JavaTimeModule());
+    MemberJoinRequest request = new MemberJoinRequest("pjh612", "123", "jhjh", "박진형",
+        LocalDate.of(1995, 05, 02), address, "pjh_jn@naver.com", "01021042419", MemberType.NORMAL);
+
+    LoginRequest loginRequest = new LoginRequest(request.getId(), request.getPwd());
+    QueryMemberDto join = memberService.join(request);
+
+    MvcResult loginResponse = mvc
+        .perform(MockMvcRequestBuilders.post(BASE_URL + "/login_jwt/" + request.getId())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(loginRequest)).with(csrf()))
+        .andExpect(status().isOk()).andDo(print()).andReturn();
+
+    final String accessToken = loginResponse.getResponse().getCookie(ACCESS_TOKEN).getValue();
+    final String refreshToken = loginResponse.getResponse().getCookie(REFRESH_TOKEN).getValue();
+    Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+    Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+
+    MvcResult logoutResult = mvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/logout_jwt/")
+        .contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(loginRequest))
+        .cookie(accessTokenCookie).cookie(refreshTokenCookie).with(csrf()))
+        .andExpect(status().isOk()).andDo(print()).andReturn();
+
+    assertNull(redisService.getStringValue(REFRESH_TOKEN));
+    assertNotNull(redisService.getStringValue(accessToken));
+  }
+
+  @DisplayName("email중복확인 테스트")
+  @Test
+  public void chkEmailDuplication() throws Exception {
+
+    Address address = new Address("busan", "guemgangro", "46233");
+    mapper.registerModule(new JavaTimeModule());
+    MemberJoinRequest request = new MemberJoinRequest("pjh612", "123", "jhjh", "박진형",
+        LocalDate.of(1995, 05, 02), address, "pjh_jn@naver.com", "01021042419", MemberType.NORMAL);
+    QueryMemberDto join = memberService.join(request);
+    mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/duplication/email")
+        .contentType(MediaType.APPLICATION_JSON).param("email", join.getEmail()).with(csrf()))
+        .andExpect(status().isConflict()).andDo(print());
+
+    String newEmail = "helloWorld@naver.com";
+    mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/duplication/email")
+        .contentType(MediaType.APPLICATION_JSON).param("email", newEmail).with(csrf()))
+        .andExpect(status().isOk()).andDo(print());
+  }
+
+  @DisplayName("id중복확인 테스트")
+  @Test
+  public void chkIdDuplication() throws Exception {
+
+    Address address = new Address("busan", "guemgangro", "46233");
+    mapper.registerModule(new JavaTimeModule());
+    MemberJoinRequest request = new MemberJoinRequest("pjh612", "123", "jhjh", "박진형",
+        LocalDate.of(1995, 05, 02), address, "pjh_jn@naver.com", "01021042419", MemberType.NORMAL);
+    QueryMemberDto join = memberService.join(request);
+    mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/duplication/id")
+        .contentType(MediaType.APPLICATION_JSON).param("id", join.getId()).with(csrf()))
+        .andExpect(status().isConflict()).andDo(print());
+
+    String newId = "lilililiil";
+    mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/duplication/id")
+        .contentType(MediaType.APPLICATION_JSON).param("id", newId).with(csrf()))
+        .andExpect(status().isOk()).andDo(print());
+  }
 }
