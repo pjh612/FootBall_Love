36847402b2b9b743120d60d01a42fdfe5e466ffa
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-02-05 03:36:41 +0900

    [feat]Paging, SpringDataJpa 적용, 댓글달기 기능 추가 등

--- football_love/src/main/java/com/deu/football_love/service/PostService.java
+++ football_love/src/main/java/com/deu/football_love/service/PostService.java
@@ -1,13 +1,67 @@
 package com.deu.football_love.service;
 
+import com.deu.football_love.domain.*;
 import com.deu.football_love.dto.post.*;
-
+import com.deu.football_love.dto.post.like.LikePostResponse;
+import com.deu.football_love.exception.LikeDuplicatedException;
+import com.deu.football_love.repository.*;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface PostService {
-    WritePostResponse writePost(WritePostRequest request);
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class PostService {
 
-    DeletePostResponse deletePost(Long postId);
+    private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final PostImageRepository postImageRepository;
+    private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
+    private final GcpStorageService gcpStorageService;
+
+    @SneakyThrows
+    public WritePostResponse writePost(WritePostRequest request) {
+        Post newPost = new Post();
+        Member findMember = memberRepository.selectMember(request.getAuthorNumber());
+        Board findBoard = boardRepository.selectBoardById(request.getBoardId());
+        newPost.setContent(request.getContent());
+        newPost.setTitle(request.getTitle());
+        newPost.setAuthor(findMember);
+        newPost.setBoard(findBoard);
+        findMember.getPosts().add(newPost);
+        findBoard.getPosts().add(newPost);
+        postRepository.save(newPost);
+        if (request.getImages() != null) {
+            for (MultipartFile image : request.getImages()) {
+                String imgUri = gcpStorageService.updatePostImg(image);
+                PostImage postImage = newPost.addPostImage(imgUri);
+                postImageRepository.save(postImage);
+            }
+        }
+        return WritePostResponse.from(newPost);
+    }
+
+    public DeletePostResponse deletePost(Long postId) {
+        Post findPost = postRepository.findById(postId).orElseThrow(()-> new IllegalArgumentException());
+        postRepository.delete(findPost);
+        return new DeletePostResponse(postId);
+    }
+
+    public ModifyPostResponse modifyPost(Long postId, UpdatePostRequest request) {
+        Post findPost = postRepository.findById(postId).orElseThrow(()-> new IllegalArgumentException());
+        findPost.update(request);
+        return new ModifyPostResponse(postId);
+    }
 
     public boolean isLiked(Long postId, Long memberNumber) {
         return postLikeRepository.existsByMemberNumberAndPostId(memberNumber, postId);
@@ -25,7 +79,48 @@ public interface PostService {
         return new LikePostResponse(true,"추천");
     }
 
-    List<QueryPostDto> findAllPostsByBoardId(Long boardId);
+    public QueryPostDto findPost(Long postId) {
+        Post findPost = postRepository.selectPostByPostId(postId).orElseThrow(()->new IllegalArgumentException());
+        QueryPostDto findPostDto = QueryPostDto.from(findPost);
+        List<QueryPostImageDto> postImages = postRepository.selectPostImagesByPostId(postId)
+                .stream()
+                .map(pi -> QueryPostImageDto.from(pi))
+                .collect(Collectors.toList());
+        findPostDto.setPostImages(postImages);
+        findPostDto.setLikeCount(postLikeRepository.countByPostId(postId));
+        return findPostDto;
+    }
 
-    List<QueryPostDto> findAllPostsByMemberId(String memberId);
+    public Page<QueryPostDto> findAllPostsByBoardId(Long boardId, Pageable pageable) {
+        Page<Post> postPageList = postRepository.selectAllPostsByBoardId(boardId, pageable);
+        Page<QueryPostDto> posts = postPageList
+                .map(p -> new QueryPostDto(p));
+        for (QueryPostDto post : posts) {
+            post.setPostImages(
+                    postRepository.selectPostImagesByPostId(post.getId())
+                            .stream()
+                            .map(pi -> QueryPostImageDto.from(pi))
+                            .collect(Collectors.toList())
+            );
+            post.setLikeCount(postLikeRepository.countByPostId(post.getId()));
+        }
+        return posts;
+    }
+
+    public List<QueryPostDto> findAllPostsByMemberId(String memberId) {
+        List<QueryPostDto> posts = postRepository.selectPostsByMemberId(memberId)
+                .stream()
+                .map(p -> new QueryPostDto(p))
+                .collect(Collectors.toList());
+        for (QueryPostDto post : posts) {
+            post.setPostImages(
+                    postRepository.selectPostImagesByPostId(post.getId())
+                            .stream()
+                            .map(pi -> QueryPostImageDto.from(pi))
+                            .collect(Collectors.toList())
+            );
+            post.setLikeCount(postLikeRepository.countByPostId(post.getId()));
+        }
+        return posts;
+    }
 }
