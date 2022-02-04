5dc16f2b3a0b58418e6657fe99ba5bd543d7b934
유시준 <simba0502@naver.com>
2022-02-04 23:26:07 +0900

    [fix] updateMemberRequest 멤버변수 수정에 따른 수정

--- football_love/src/main/java/com/deu/football_love/domain/Member.java
+++ football_love/src/main/java/com/deu/football_love/domain/Member.java
@@ -1,13 +1,23 @@
 package com.deu.football_love.domain;
 
-import com.deu.football_love.domain.type.MemberType;
-import com.deu.football_love.dto.member.UpdateMemberRequest;
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import com.deu.football_love.domain.type.MemberType;
+import com.deu.football_love.dto.member.UpdateMemberRequest;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -15,87 +25,85 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "member_number")
-    private Long number;
+  @Id
+  @GeneratedValue
+  @Column(name = "member_number")
+  private Long number;
 
 
-    @Column(name = "member_id", length = 20, unique = true)
-    private String id;
+  @Column(name = "member_id", length = 20, unique = true)
+  private String id;
 
-    @Column(name = "member_pwd", length = 150)
-    private String pwd;
+  @Column(name = "member_pwd", length = 150)
+  private String pwd;
 
-    @Column(name = "member_nickname", length = 20)
-    private String nickname;
+  @Column(name = "member_nickname", length = 20)
+  private String nickname;
 
-    @Column(name = "member_name", length = 20)
-    private String name;
+  @Column(name = "member_name", length = 20)
+  private String name;
 
-    @Column(name = "member_birth")
-    private LocalDate birth;
+  @Column(name = "member_birth")
+  private LocalDate birth;
 
-    @Column(name = "member_address")
-    @Embedded
-    private Address address;
+  @Column(name = "member_address")
+  @Embedded
+  private Address address;
 
-    @Column(name = "member_email", unique = true)
-    private String email;
+  @Column(name = "member_email", unique = true)
+  private String email;
 
-    @Column(name = "member_phone")
-    private String phone;
+  @Column(name = "member_phone")
+  private String phone;
 
-    @Column(name = "member_type")
-    private MemberType memberType;
+  @Column(name = "member_type")
+  private MemberType memberType;
 
-    @Column(name = "member_profile_img_uri")
-    private String profileImgUri;
+  @Column(name = "member_profile_img_uri")
+  private String profileImgUri;
 
-    @OneToMany(mappedBy = "author")
-    private List<Post> posts = new ArrayList<>();
+  @OneToMany(mappedBy = "author")
+  private List<Post> posts = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<TeamMember> teamMembers = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<TeamMember> teamMembers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<ParticipationMember> participationMembers = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<ParticipationMember> participationMembers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<ApplicationJoinTeam> applicationJoinTeams = new ArrayList<>();
+  @OneToMany(mappedBy = "member")
+  private List<ApplicationJoinTeam> applicationJoinTeams = new ArrayList<>();
 
-    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL)
-    private WithdrawalMember withdrawalMember;
+  @OneToOne(mappedBy = "member", cascade = CascadeType.ALL)
+  private WithdrawalMember withdrawalMember;
 
-    @OneToOne(mappedBy = "owner", orphanRemoval = true)
-    private Company company;
+  @OneToOne(mappedBy = "owner", orphanRemoval = true)
+  private Company company;
 
-    @Builder(builderClassName = "MemberBuilder",
-            builderMethodName = "memberBuilder")
-    public Member(Long number, String id, String pwd, String nickname, String name, LocalDate birth, Address address, String email, String phone, MemberType memberType) {
-        this.number = number;
-        this.id = id;
-        this.pwd = pwd;
-        this.nickname = nickname;
-        this.name = name;
-        this.birth = birth;
-        this.address = address;
-        this.email = email;
-        this.phone = phone;
-        this.memberType = memberType;
-    }
+  @Builder(builderClassName = "MemberBuilder", builderMethodName = "memberBuilder")
+  public Member(Long number, String id, String pwd, String nickname, String name, LocalDate birth,
+      Address address, String email, String phone, MemberType memberType) {
+    this.number = number;
+    this.id = id;
+    this.pwd = pwd;
+    this.nickname = nickname;
+    this.name = name;
+    this.birth = birth;
+    this.address = address;
+    this.email = email;
+    this.phone = phone;
+    this.memberType = memberType;
+  }
 
-    public void updateMember(UpdateMemberRequest request) {
-        pwd = request.getPwd();
-        email = request.getEmail();
-        name = request.getName();
-        nickname = request.getNickname();
-        birth = request.getBirth();
-        address = request.getAddress();
-        phone = request.getPhone();
-    }
+  public void updateMember(UpdateMemberRequest request) {
+    pwd = request.getPwd();
+    email = request.getEmail();
+    nickname = request.getNickname();
+    address = request.getAddress();
+    phone = request.getPhone();
+  }
 
-    public void updateProfileImgUri(String uri) {
-        profileImgUri = uri;
-    }
+  public void updateProfileImgUri(String uri) {
+    profileImgUri = uri;
+  }
 }
