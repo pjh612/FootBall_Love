35938828f37c42fced4ceb170ad185fbdf24a400
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-02-14 22:12:41 +0900

    [feat]QueryMemberDto from함수 및 생성자 추가

--- football_love/src/main/java/com/deu/football_love/dto/member/QueryMemberDto.java
+++ football_love/src/main/java/com/deu/football_love/dto/member/QueryMemberDto.java
@@ -1,17 +1,20 @@
 package com.deu.football_love.dto.member;
 
+import com.deu.football_love.domain.Company;
+import com.deu.football_love.dto.BaseDto;
 import java.time.LocalDate;
 import com.deu.football_love.domain.Address;
 import com.deu.football_love.domain.BaseEntity;
 import com.deu.football_love.domain.Member;
 import com.deu.football_love.domain.type.MemberType;
 import com.deu.football_love.dto.company.QueryCompanyDto;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class QueryMemberDto extends BaseEntity {
+public class QueryMemberDto extends BaseDto {
 
   private Long number;
 
@@ -35,6 +38,30 @@ public class QueryMemberDto extends BaseEntity {
 
   private QueryCompanyDto company;
 
+
+  public QueryMemberDto(Long number, String id, String nickname, String name, String email,
+      LocalDate birth, Address address, String phone, MemberType type, String profileUri,
+      Company company, Long createdBy, Long lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+    this.number =number;
+    this.id = id;
+    this.nickname = nickname;
+    this.name = name;
+    this.email = email;
+    this.birth = birth;
+    this.address = address;
+    this.phone = phone;
+    this.type = type;
+    this.profileUri = profileUri;
+    if (company != null) {
+      this.company = QueryCompanyDto.from(company);
+    }
+    setCreatedBy(createdBy);
+    setLastModifiedBy(lastModifiedBy);
+    setCreatedDate(createdDate);
+    setLastModifiedDate(lastModifiedDate);
+
+  }
+
   public QueryMemberDto(Member member) {
     this.number = member.getNumber();
     this.id = member.getId();
@@ -46,12 +73,18 @@ public class QueryMemberDto extends BaseEntity {
     this.phone = member.getPhone();
     this.type = member.getMemberType();
     this.profileUri = member.getProfileImgUri();
-    if (member.getCompany() != null)
+    if (member.getCompany() != null) {
       this.company = QueryCompanyDto.from(member.getCompany());
+    }
     setCreatedBy(member.getCreatedBy());
     setLastModifiedBy(member.getLastModifiedBy());
     setCreatedDate(member.getCreatedDate());
     setLastModifiedDate(member.getLastModifiedDate());
 
+  }
+
+  public static QueryMemberDto from(Member member)
+  {
+    return new QueryMemberDto(member);
   }
 }
