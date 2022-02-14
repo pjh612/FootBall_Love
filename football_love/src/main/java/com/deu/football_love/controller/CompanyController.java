a3d9769df0b5fa3eaf0712a5c02c285b56da87d3
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-02-14 22:11:48 +0900

    [feat]Company Profile Update 기능, Company로 등록된 매치 조회 기능 추가

--- football_love/src/main/java/com/deu/football_love/controller/CompanyController.java
+++ football_love/src/main/java/com/deu/football_love/controller/CompanyController.java
@@ -1,36 +1,68 @@
 package com.deu.football_love.controller;
 
 
+import com.deu.football_love.dto.auth.LoginInfo;
 import com.deu.football_love.dto.company.AddCompanyRequest;
 import com.deu.football_love.dto.company.AddCompanyResponse;
+import com.deu.football_love.dto.company.UpdateCompanyRequest;
 import com.deu.football_love.dto.company.WithdrawalCompanyRequest;
 import com.deu.football_love.dto.company.WithdrawalCompanyResponse;
 import com.deu.football_love.service.CompanyService;
+import com.deu.football_love.service.MatchService;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.Valid;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/company")
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
-    private final CompanyService companyService;
 
-    @PostMapping
-    public ResponseEntity add(@Valid @RequestBody AddCompanyRequest request)
-    {
-        AddCompanyResponse response = companyService.addCompany(request.getCompanyName(), request.getOwnerNumber(), request.getLocation(), request.getTel(), request.getDescription());
-        return new ResponseEntity(response, HttpStatus.OK);
-    }
+  private final CompanyService companyService;
+  private final MatchService matchService;
 
-    @DeleteMapping
-    public ResponseEntity withdrawal(@Valid @RequestBody WithdrawalCompanyRequest request)
-    {
-        WithdrawalCompanyResponse response = companyService.withdrawalCompany(request.getCompanyId());
-        return new ResponseEntity(response, HttpStatus.OK);
-    }
+  @PostMapping
+  @PreAuthorize("hasRole('BUSINESS')")
+  public ResponseEntity add(@AuthenticationPrincipal LoginInfo loginInfo,
+      @Valid @RequestBody AddCompanyRequest request) {
+    AddCompanyResponse response = companyService
+        .addCompany(request.getCompanyName(), loginInfo.getNumber(), request.getLocation(),
+            request.getTel(), request.getDescription());
+    return new ResponseEntity(response, HttpStatus.OK);
+  }
+
+  @DeleteMapping
+  public ResponseEntity withdrawal(@Valid @RequestBody WithdrawalCompanyRequest request) {
+    WithdrawalCompanyResponse response = companyService.withdrawalCompany(request.getCompanyId());
+    return new ResponseEntity(response, HttpStatus.OK);
+  }
+
+
+  @GetMapping("/match")
+  @PreAuthorize("hasRole('BUSINESS')")
+  public ResponseEntity getCompanyMatchList(@AuthenticationPrincipal LoginInfo loginInfo) {
+    return new ResponseEntity(matchService.findAllByCompanyId(loginInfo.getNumber()),
+        HttpStatus.OK);
+  }
+
+  @PutMapping
+  @PreAuthorize("hasRole('BUSINESS')")
+  public ResponseEntity update(@RequestBody UpdateCompanyRequest request, @AuthenticationPrincipal LoginInfo loginInfo) {
+    companyService.updateCompany(request);
+    return new ResponseEntity(HttpStatus.OK);
+  }
+
+  @ExceptionHandler({IllegalArgumentException.class})
+  public ResponseEntity handleDataNotFoundException(final IllegalArgumentException ex) {
+    log.info(ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
 }
