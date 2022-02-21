b4faf71bfff5060685dbfbf2a30f17745e0e4c99
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-02-22 00:18:31 +0900

    [fix] 팀 생성 응답에는 팀 Name을 추가로 포함하도록 변경

--- football_love/src/main/java/com/deu/football_love/dto/team/CreateTeamResponse.java
+++ football_love/src/main/java/com/deu/football_love/dto/team/CreateTeamResponse.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public class CreateTeamResponse {
     private Long teamId;
+    private String teamName;
 
-    public CreateTeamResponse(Long teamId) {
+    public CreateTeamResponse(Long teamId, String teamName) {
         this.teamId = teamId;
+        this.teamName = teamName;
     }
 }
