a5e721812916da74429424d076a58f93c4ca60a9
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-02-22 00:13:30 +0900

    [fix]기본 생성사 및 생성자 추가

--- football_love/src/main/java/com/deu/football_love/dto/match/ApplyMatchRequest.java
+++ football_love/src/main/java/com/deu/football_love/dto/match/ApplyMatchRequest.java
@@ -3,9 +3,12 @@ package com.deu.football_love.dto.match;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApplyMatchRequest {
 	@Positive
 	@NotNull
@@ -14,4 +17,9 @@ public class ApplyMatchRequest {
 	@Positive
 	@NotNull
 	Long matchId;
+
+	public ApplyMatchRequest(Long teamId, Long matchId) {
+		this.teamId = teamId;
+		this.matchId = matchId;
+	}
 }
