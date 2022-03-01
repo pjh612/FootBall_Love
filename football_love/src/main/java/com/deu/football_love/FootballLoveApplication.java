723de808100587dad5f5a48b4d29c33bac0d5252
pjh612 <62292492+pjh612@users.noreply.github.com>
2022-03-01 15:41:51 +0900

    [test]자동 배포 테스트

--- football_love/src/main/java/com/deu/football_love/FootballLoveApplication.java
+++ football_love/src/main/java/com/deu/football_love/FootballLoveApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FootballLoveApplication {
 
     public static void main(String[] args) {
-        //github action test2
+        //github action test3
         SpringApplication.run(FootballLoveApplication.class, args);
     }
 }
