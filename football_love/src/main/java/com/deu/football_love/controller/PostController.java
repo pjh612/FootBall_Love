f91a6686764382f46fbc8758a6ba291a492d91ef
유시준 <simba0502@naver.com>
2022-03-28 22:16:15 +0900

    Controller 요청 post와 teampost로 요청구조 변경 및 공지사항 작성 API추가

--- football_love/src/main/java/com/deu/football_love/controller/PostController.java
+++ football_love/src/main/java/com/deu/football_love/controller/PostController.java
@@ -1,23 +1,37 @@
 package com.deu.football_love.controller;
 
-import com.deu.football_love.domain.type.TeamMemberType;
-import com.deu.football_love.dto.auth.LoginInfo;
-import com.deu.football_love.dto.post.*;
-import com.deu.football_love.dto.post.like.LikePostResponse;
-import com.deu.football_love.exception.CustomException;
-import com.deu.football_love.exception.NotTeamMemberException;
-import com.deu.football_love.exception.error_code.ErrorCode;
-import com.deu.football_love.service.*;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.deu.football_love.domain.type.TeamMemberType;
+import com.deu.football_love.dto.auth.LoginInfo;
+import com.deu.football_love.dto.post.DeletePostResponse;
+import com.deu.football_love.dto.post.QueryPostDto;
+import com.deu.football_love.dto.post.UpdatePostRequest;
+import com.deu.football_love.dto.post.WritePostRequest;
+import com.deu.football_love.dto.post.WritePostResponse;
+import com.deu.football_love.dto.post.WriteTeamPostRequest;
+import com.deu.football_love.dto.post.like.LikePostResponse;
+import com.deu.football_love.exception.CustomException;
+import com.deu.football_love.exception.NotTeamMemberException;
+import com.deu.football_love.exception.error_code.ErrorCode;
+import com.deu.football_love.service.PostService;
+import com.deu.football_love.service.TeamService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,16 +43,26 @@ public class PostController {
   private final TeamService teamService;
 
   /**
-   * 게시글 생성
+   * 팀게시판 게시글 생성
    */
-  @PostMapping("/board/post")
-  public ResponseEntity<WritePostResponse> writePost(WritePostRequest writePostRequest, @AuthenticationPrincipal LoginInfo loginInfo) {
+  @PostMapping("/board/team/post")
+  public ResponseEntity<WritePostResponse> writeTeamPost(WriteTeamPostRequest writePostRequest, @AuthenticationPrincipal LoginInfo loginInfo) {
     TeamMemberType teamMemberType = teamService.authorityCheck(writePostRequest.getTeamId(), loginInfo.getNumber());
     if (teamMemberType == TeamMemberType.NONE) {
       throw new NotTeamMemberException();
     }
+    WritePostResponse writePostResponse = postService.writeTeamPost(writePostRequest, loginInfo.getNumber());
+    return new ResponseEntity<WritePostResponse>(writePostResponse, HttpStatus.OK);
+  }
+
+  /**
+   * 공용 게시글 생성
+   */
+  @PostMapping("/board/post")
+  public ResponseEntity<WritePostResponse> writePost(WritePostRequest writePostRequest, @AuthenticationPrincipal LoginInfo loginInfo)
+      throws IOException {
     WritePostResponse writePostResponse = postService.writePost(writePostRequest, loginInfo.getNumber());
-    return new ResponseEntity(writePostResponse, HttpStatus.OK);
+    return new ResponseEntity<WritePostResponse>(writePostResponse, HttpStatus.OK);
   }
 
   /**
